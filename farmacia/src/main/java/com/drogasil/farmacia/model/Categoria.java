28d9fd349aa457617e0a8c52aa9bbfe2d245a3e2
Cinthia <cinthia.tengan@gmail.com>
2020-12-09 14:57:53 -0300

    Upload Exercise 9-12-2020

---
+++ farmacia/src/main/java/com/drogasil/farmacia/model/Categoria.java
@@ -0,0 +1,43 @@
+package com.drogasil.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "farmacia")
+public class Categoria {
+	//attributes
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@NotNull
+	@Size(min = 10, max = 50)
+	private String descricao;
+	@NotNull
+	private boolean ativo;
+	//getters and setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+}
---
+++ farmacia/src/test/java/com/drogasil/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drogasil.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
